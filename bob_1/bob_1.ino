40a84d78101b12a4aa2140f0fe35e87a3029cc09
Robert Gardner <rhg259@nyu.edu>
2015-01-16 21:18:58 -0500

    Initial commit

---
+++ bob_1/bob_1.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+#define NUM_PIXELS 120
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, PIN);
+
+int delayval = 30; // delay for half a second
+
+void setup() {
+  strip.begin();
+  strip.show();
+}
+
+void loop() {
+  int offset = NUM_PIXELS / 2;
+  for (int i = 0; i < offset; i++){
+    strip.setPixelColor(i, strip.Color(40, 40, 40));
+    strip.setPixelColor(i + offset, strip.Color(63, 63, 63));
+    strip.show();
+    delay(delayval);
+  }
+  for (int i = offset; i >= 0; i--) {
+    strip.setPixelColor(i, strip.Color(0, 0, 0));
+    strip.setPixelColor(i + offset, strip.Color(0, 0, 0));
+    strip.show();
+    delay(delayval);
+  }
+}
